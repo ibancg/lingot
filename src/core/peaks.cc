3eed8b95c8f5cf1c2add35887a1dd2c75a74b21e
Ibán Cereijo <ibancg@users.noreply.github.com>
2005-07-25 01:36:46 +0200

    lingot 0.6.1

--- src/core/peaks.cc
+++ src/core/peaks.cc
@@ -2,7 +2,7 @@
 /*
   lingot, a musical instrument tuner.
 
-  Copyright (C) 2004   Ibán Cereijo Graña, Jairo Chapela Martínez.
+  Copyright (C) 2004, 2005  Ibán Cereijo Graña, Jairo Chapela Martínez.
 
   This file is part of lingot.
 
