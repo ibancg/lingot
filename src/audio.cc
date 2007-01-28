c5cabfe5ccaa039d3d270ab2ff860dcf4f0513b8
Jairo Chapela-Martínez <jairochapela@users.noreply.github.com>
2007-01-28 05:26:52 +0100

    Anjutized.

---
+++ src/audio.cc
@@ -0,0 +1,122 @@
+//-*- C++ -*-
+/*
+  lingot, a musical instrument tuner.
+
+  Copyright (C) 2004, 2005  Ibán Cereijo Graña, Jairo Chapela Martínez.
+
+  This file is part of lingot.
+
+  lingot is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  lingot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with lingot; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <sys/soundcard.h>
+#include <stdlib.h>
+
+#include "defs.h"
+#include "audio.h"
+
+audio::audio(int channels, int rate, int format, char* fdsp)
+{
+  //  int dma  = 4;
+
+  dsp = open(fdsp, O_RDONLY);
+  if (dsp < 0)
+    {
+      char buff[80];
+      sprintf(buff, "Unable to open audio device %s", fdsp);
+      perror(buff);
+      exit(-1);
+    }
+  
+  //if (ioctl(dsp, SOUND_PCM_READ_CHANNELS, &channels) < 0)
+  if (ioctl(dsp, SNDCTL_DSP_CHANNELS, &channels) < 0)
+    {
+      perror("Error setting number of channels");
+      exit(-1);
+    }
+  
+  /*  if (ioctl(dsp, SOUND_PCM_WRITE_CHANNELS, &channels) < 0)
+      {
+      perror("Error setting number of channels");
+      exit(-1);
+      }*/
+  
+  // sample size
+  //if (ioctl(dsp, SOUND_PCM_SETFMT, &format) < 0)
+  if (ioctl(dsp, SNDCTL_DSP_SETFMT, &format) < 0)
+    {
+      perror("Error setting bits per sample");
+      exit(-1);
+    }
+
+  int fragment_size = 1;
+  int DMA_buffer_size = 512;
+  int param = 0;
+
+  for (param = 0; fragment_size < DMA_buffer_size; param++)
+    fragment_size <<= 1;
+
+  param |= 0x00ff0000;
+
+  if (ioctl(dsp, SNDCTL_DSP_SETFRAGMENT, &param) < 0)
+    {
+      perror("Error setting DMA buffer size");
+      exit(-1);
+    }
+
+  // DMA divisor
+  /*  if (ioctl(dsp, SNDCTL_DSP_SUBDIVIDE, &dma) < 0)
+      {
+      perror("Error setting DMA divisor");
+      exit(-1);
+      }
+  
+      // Rate de muestreo / reproduccion
+      if (ioctl(dsp, SOUND_PCM_WRITE_RATE, &rate) < 0)
+      {
+      perror("Error setting write rate");
+      exit(-1);
+      }*/
+
+  //  if (ioctl(dsp, SOUND_PCM_READ_RATE, &rate) < 0)
+  if (ioctl(dsp, SNDCTL_DSP_SPEED, &rate) < 0)
+    {
+      perror("Error setting sample rate");
+      exit(-1);
+    }
+
+  /*
+  // non-blocking reads.
+  if (fcntl(dsp, F_SETFL, O_NONBLOCK) < 0)
+  {
+  perror("Error setting non-blocking reads");
+  exit(-1);
+  }
+  */
+}
+
+audio::~audio()
+{
+  close(dsp);
+}
+
+int audio::read(void *buffer, int size)
+{
+  return ::read(dsp, buffer, size);
+}
---
+++ src/dialog_config.h
@@ -0,0 +1,81 @@
+//-*- C++ -*-
+/*
+  lingot, a musical instrument tuner.
+
+  Copyright (C) 2004, 2005  Ibán Cereijo Graña, Jairo Chapela Martínez.
+
+  This file is part of lingot.
+
+  lingot is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  lingot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with lingot; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef __DIALOG_CONFIG_H__
+#define __DIALOG_CONFIG_H__
+
+class GUI;
+
+/*--------- Dialog box with multiple options --------------------*/
+
+class DialogConfig {
+
+private:
+
+  GtkWidget*	win; // window
+
+	// widgets that contains configuration information.
+  GtkWidget*	spin_calculation_rate;
+  GtkWidget*	spin_visualization_rate;
+  GtkWidget*	spin_oversampling;
+  GtkWidget*	spin_root_frequency_error;  
+  GtkWidget*	spin_temporal_window;
+  GtkWidget*	spin_noise_threshold;
+  GtkWidget*	spin_dft_number;
+  GtkWidget*	spin_dft_size;
+  GtkWidget*	spin_peak_number;
+  GtkWidget*	spin_peak_order;
+  GtkWidget*	spin_peak_rejection_relation;
+  GtkWidget*	combo_fft_size;
+  GtkWidget*	combo_sample_rate;
+
+  GtkWidget*	button_ok;
+  GtkWidget*	button_cancel;
+  GtkWidget*	button_apply;
+  GtkWidget*	button_default;
+  
+  GtkWidget*	label_sample_rate;
+  GtkWidget*	label_root_frequency;
+
+  Config*			conf;     // provisional configuration.
+  Config*			conf_old; // restoration point for cancel.
+
+  GUI*				gui;
+
+public:
+
+	DialogConfig(GUI*);
+	virtual ~DialogConfig();
+	
+  void rewrite();
+  void apply();
+  void close();
+  
+  friend void clicked_cb(GtkButton *b, DialogConfig *d);
+  
+  friend void change_label_sample_rate( GtkWidget *widget, DialogConfig *d);
+  friend void change_label_a_frequence( GtkWidget *widget, DialogConfig *d);
+};
+
+
+#endif
