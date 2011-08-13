46d8fab786cec62b4681c86e11895f72f8191d82
Ibán Cereijo <ibancg@users.noreply.github.com>
2011-08-13 18:52:44 +0200

    Fixed bug #34007.

--- src/lingot.c
+++ src/lingot.c
@@ -40,38 +40,38 @@
 
 char CONFIG_FILE_NAME[100];
 
-void print_card_list(void) {
-   int status;
-   int card = -1;  // use -1 to prime the pump of iterating through card list
-   char* longname  = NULL;
-   char* shortname = NULL;
-
-   if ((status = snd_card_next(&card)) < 0) {
-      error("cannot determine card number: %s", snd_strerror(status));
-      return;
-   }
-   if (card < 0) {
-      error("no sound cards found");
-      return;
-   }
-   while (card >= 0) {
-      printf("Card %d:", card);
-      if ((status = snd_card_get_name(card, &shortname)) < 0) {
-         error("cannot determine card shortname: %s", snd_strerror(status));
-         break;
-      }
-      if ((status = snd_card_get_longname(card, &longname)) < 0) {
-         error("cannot determine card longname: %s", snd_strerror(status));
-         break;
-      }
-      printf("\tLONG NAME:  %s\n", longname);
-      printf("\tSHORT NAME: %s\n", shortname);
-      if ((status = snd_card_next(&card)) < 0) {
-         error("cannot determine card number: %s", snd_strerror(status));
-         break;
-      }
-   }
-}
+//void print_card_list(void) {
+//   int status;
+//   int card = -1;  // use -1 to prime the pump of iterating through card list
+//   char* longname  = NULL;
+//   char* shortname = NULL;
+//
+//   if ((status = snd_card_next(&card)) < 0) {
+//      error("cannot determine card number: %s", snd_strerror(status));
+//      return;
+//   }
+//   if (card < 0) {
+//      error("no sound cards found");
+//      return;
+//   }
+//   while (card >= 0) {
+//      printf("Card %d:", card);
+//      if ((status = snd_card_get_name(card, &shortname)) < 0) {
+//         error("cannot determine card shortname: %s", snd_strerror(status));
+//         break;
+//      }
+//      if ((status = snd_card_get_longname(card, &longname)) < 0) {
+//         error("cannot determine card longname: %s", snd_strerror(status));
+//         break;
+//      }
+//      printf("\tLONG NAME:  %s\n", longname);
+//      printf("\tSHORT NAME: %s\n", shortname);
+//      if ((status = snd_card_next(&card)) < 0) {
+//         error("cannot determine card number: %s", snd_strerror(status));
+//         break;
+//      }
+//   }
+//}
 
 int main(int argc, char *argv[]) {
 
