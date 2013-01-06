d76b88fe482b1fa93ccdb26f93bc588ad0deecb0
Ibán Cereijo <ibancg@users.noreply.github.com>
2013-01-07 00:36:49 +0100

    Implemented moving average filter for computing noise level.

---
+++ test/src/lingot-signal-test.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+
+#include "lingot-signal.h"
+#include "lingot-signal.c"
+
+int lingot_signal_test() {
+
+	const int N = 16;
+	int i = 0;
+	FLT* spd = malloc(N * sizeof(FLT));
+	FLT* noise = malloc(N * sizeof(FLT));
+
+	for (i = 0; i < N; i++) {
+		spd[i] = i + 1;
+		noise[i] = -1.0;
+	}
+
+	lingot_signal_compute_noise_level(spd, N, 5, noise);
+
+	printf("S = [");
+	for (i = 0; i < N; i++) {
+		printf(" %f ", spd[i]);
+	}
+	printf("] \n");
+	printf("N = [");
+	for (i = 0; i < N; i++) {
+		printf(" %f ", noise[i]);
+	}
+	printf("] \n");
+
+	free(spd);
+	free(noise);
+
+	puts("done.");
+	return 0;
+}
---
+++ test/src/lingot-signal-test.h
@@ -0,0 +1,1 @@
+int lingot_signal_test();
