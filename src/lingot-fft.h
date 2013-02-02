9089b688c286dd571e3ce6ffd8d69afab12fb9cd
Ibán Cereijo <ibancg@users.noreply.github.com>
2013-02-02 14:47:05 +0100

    Trying final NR iteration over the windowed FFT-sized temporal buffer instead of the whole temporal buffer. Good response. Recovered some old signal processing functions for comparison purposes.

--- src/lingot-fft.h
+++ src/lingot-fft.h
@@ -57,8 +57,11 @@ void lingot_fft_plan_destroy(LingotFFTPlan*);
 // Full Spectral Power Distribution (SPD) esteem.
 void lingot_fft_compute_dft_and_spd(LingotFFTPlan*, FLT* out, int n_out);
 
+// Spectral Power Distribution (SPD) evaluation at a given frequency.
+void lingot_fft_spd_eval(FLT* in, int N1, FLT wi, FLT dw, FLT* out, int N2);
+
 // Evaluates first and second SPD derivatives at frequency w.
-void lingot_fft_spd_diffs_eval(const FLT* in, int N, FLT w, FLT* out_d1,
-		FLT* out_d2);
+void lingot_fft_spd_diffs_eval(const FLT* in, int N, FLT w, FLT* out_d0,
+		FLT* out_d1, FLT* out_d2);
 
 #endif
