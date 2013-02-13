7e44f0d0cdadad7c6c1fd4e4b576f251e4cb4b48
ibancereijo <ibancereijo@localhost>
2013-02-13 16:11:54 +0100

    Using CUnit.

--- test/src/lingot-config-scale-test.c
+++ test/src/lingot-config-scale-test.c
@@ -27,25 +27,57 @@
 
 void lingot_config_scale_test() {
 
-	initTestCase("lingot_config_scale_test");
-
 	lingot_config_create_parameter_specs();
 	LingotConfig* config = lingot_config_new();
 	lingot_config_restore_default_values(config);
 	LingotScale* scale = config->scale;
 
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, 0), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, 1), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, 10), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, 11), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, 12), 1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, 23), 1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, 24), 2);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -1), -1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -10), -1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -11), -1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -12), -1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -13), -2);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -23), -2);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -24), -2);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_octave(scale, -25), -3);
+
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, 0), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, 1), 1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, 10), 10);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, 11), 11);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, 12), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, 23), 11);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, 24), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, -1), 11);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, -2), 10);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, -11), 1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, -12), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, -23), 1);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, -24), 0);
+	CU_ASSERT_EQUAL(lingot_config_scale_get_note_index(scale, -25), 11);
+
 	double error_cents;
-	int note_index = 0;
 	int closest_note_index = 0;
+
+	closest_note_index = lingot_config_scale_get_closest_note_index(scale,
+			140.812782, 0.0, &error_cents);
+	CU_ASSERT_EQUAL(closest_note_index, -11);
 	closest_note_index = lingot_config_scale_get_closest_note_index(scale,
 			130.812782, 0.0, &error_cents);
-	printf("closest note %i\n", closest_note_index);
-	assert(closest_note_index == -12);
+	CU_ASSERT_EQUAL(closest_note_index, -12);
 	closest_note_index = lingot_config_scale_get_closest_note_index(scale,
 			130.81, 0.0, &error_cents);
-	assert(closest_note_index == -12);
+	CU_ASSERT_EQUAL(closest_note_index, -12);
+	closest_note_index = lingot_config_scale_get_closest_note_index(scale,
+			130.82, 0.0, &error_cents);
+	CU_ASSERT_EQUAL(closest_note_index, -12);
 
 	lingot_config_destroy(config);
-
-	finishTestCase();
 }
--- test/src/lingot-config-test.c
+++ test/src/lingot-config-test.c
@@ -27,8 +27,6 @@
 
 void lingot_config_test() {
 
-	initTestCase("lingot_config_test");
-
 	lingot_config_create_parameter_specs();
 	LingotConfig* config = lingot_config_new();
 
@@ -36,26 +34,24 @@ void lingot_config_test() {
 
 	lingot_config_load(config, "resources/lingot-001.conf");
 
-	assert((config->audio_system == AUDIO_SYSTEM_PULSEAUDIO));
-	assert(
+	CU_ASSERT_EQUAL(config->audio_system, AUDIO_SYSTEM_PULSEAUDIO);
+	CU_ASSERT(
 			!strcmp(config->audio_dev[config->audio_system], "alsa_input.pci-0000_00_1b.0.analog-stereo"));
-	assert((config->sample_rate == 44100));
-	assert((config->oversampling == 25));
-	assert((config->root_frequency_error == 0.0));
-	assert((config->min_frequency == 15.0));
-	assert((config->sample_rate == 44100));
-	assert((config->fft_size == 512));
-	assert((config->temporal_window == ((FLT) 0.32)));
-	assert((config->noise_threshold_db == 20.0));
-	assert((config->calculation_rate == 20.0));
-	assert((config->visualization_rate == 30.0));
-	assert((config->peak_number == 3));
-	assert((config->peak_half_width == 1));
-	assert((config->peak_rejection_relation_db == 20.0));
-	assert((config->dft_number == 2));
-	assert((config->dft_size == 15));
+	CU_ASSERT_EQUAL(config->sample_rate, 44100);
+	CU_ASSERT_EQUAL(config->oversampling, 25);
+	CU_ASSERT_EQUAL(config->root_frequency_error, 0.0);
+	CU_ASSERT_EQUAL(config->min_frequency, 15.0);
+	CU_ASSERT_EQUAL(config->sample_rate, 44100);
+	CU_ASSERT_EQUAL(config->fft_size, 512);
+	CU_ASSERT_EQUAL(config->temporal_window, ((FLT) 0.32));
+	CU_ASSERT_EQUAL(config->noise_threshold_db, 20.0);
+	CU_ASSERT_EQUAL(config->calculation_rate, 20.0);
+	CU_ASSERT_EQUAL(config->visualization_rate, 30.0);
+	CU_ASSERT_EQUAL(config->peak_number, 3);
+	CU_ASSERT_EQUAL(config->peak_half_width, 1);
+	CU_ASSERT_EQUAL(config->peak_rejection_relation_db, 20.0);
+	CU_ASSERT_EQUAL(config->dft_number, 2);
+	CU_ASSERT_EQUAL(config->dft_size, 15);
 
 	lingot_config_destroy(config);
-
-	finishTestCase();
 }
