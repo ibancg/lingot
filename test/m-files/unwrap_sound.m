2c5c5d9e4db1dcd60490eba8c8b9a14cf40dd377
Iban Cereijo <ibancg@users.noreply.github.com>
2018-07-08 13:17:38 +0200

    Next development iteration.

--- test/m-files/unwrap_sound.m
+++ test/m-files/unwrap_sound.m
@@ -1,3 +1,35 @@
+%
+% lingot, a musical instrument tuner.
+%
+% Copyright (C) 2013  Iban Cereijo
+%
+% This file is part of lingot.
+%
+% lingot is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% lingot is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with lingot; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+%
+% This file contains some signal processing functions in Octave in order
+% to debug signals captured with Lingot.
+%
+% This file is not intended to be part of the unit tests nor is it intended
+% to be distributed.
+%
+
+
 % representable pieces
 delta_t = 0.02;
 delta_f = 1800;
