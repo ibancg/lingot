03e7b255f906010d9a4786adc14297e45bbb4020
Iban Cereijo <ibancg@users.noreply.github.com>
2020-05-09 00:46:12 +0200

    Rolled back tests to use tmpnam(), in order to pass CI tests.

--- test/src/lingot-test-io-config.c
+++ test/src/lingot-test-io-config.c
@@ -32,8 +32,6 @@
 
 #include <memory.h>
 #include <locale.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 #ifndef LINGOT_TEST_RESOURCES_PATH
 #define LINGOT_TEST_RESOURCES_PATH "test/"
@@ -75,13 +73,6 @@ static void check_last_configs(const lingot_config_t* config) {
 
 void lingot_test_io_config(void) {
 
-    lingot_io_config_create_parameter_specs();
-    lingot_config_t _config;
-    lingot_config_t* config = &_config;
-    lingot_config_new(config);
-
-    CU_ASSERT_PTR_NOT_NULL_FATAL(config);
-
     // check if the files are there
     const char* filename = LINGOT_TEST_RESOURCES_PATH "resources/lingot-001.conf";
 
@@ -93,88 +84,79 @@ void lingot_test_io_config(void) {
         return; // we allow the test resources to be not present for the distcheck.
     }
 
+    lingot_io_config_create_parameter_specs();
+    lingot_config_t _config;
+    lingot_config_t* config = &_config;
+    lingot_config_new(config);
+
+    CU_ASSERT_PTR_NOT_NULL_FATAL(config);
+
     // old file with obsolete options
     // ------------------------------
 
     int ok;
     ok = lingot_io_config_load(config, LINGOT_TEST_RESOURCES_PATH "resources/lingot-001.conf");
+    CU_ASSERT(ok);
 
-    if (ok) { // TODO: assert
-
-        CU_ASSERT_EQUAL(config->audio_system_index, lingot_audio_system_find_by_name("PulseAudio"));
-        CU_ASSERT(!strcmp(config->audio_dev[config->audio_system_index],
-                  "alsa_input.pci-0000_00_1b.0.analog-stereo"));
-        CU_ASSERT_EQUAL(config->sample_rate, 44100);
-        CU_ASSERT_EQUAL(config->root_frequency_error, 0.0);
-        CU_ASSERT_EQUAL(config->sample_rate, 44100);
-        CU_ASSERT_EQUAL(config->fft_size, 512);
-        CU_ASSERT_EQUAL(config->min_overall_SNR, 20.0);
-        CU_ASSERT_EQUAL(config->calculation_rate, 20.0);
-    } else {
-        fprintf(stderr, "warning: cannot find unit test resources\n");
-    }
+    CU_ASSERT_EQUAL(config->audio_system_index, lingot_audio_system_find_by_name("PulseAudio"));
+    CU_ASSERT(!strcmp(config->audio_dev[config->audio_system_index],
+              "alsa_input.pci-0000_00_1b.0.analog-stereo"));
+    CU_ASSERT_EQUAL(config->sample_rate, 44100);
+    CU_ASSERT_EQUAL(config->root_frequency_error, 0.0);
+    CU_ASSERT_EQUAL(config->sample_rate, 44100);
+    CU_ASSERT_EQUAL(config->fft_size, 512);
+    CU_ASSERT_EQUAL(config->min_overall_SNR, 20.0);
+    CU_ASSERT_EQUAL(config->calculation_rate, 20.0);
 
     // recent file
     // -----------
 
     ok = lingot_io_config_load(config, LINGOT_TEST_RESOURCES_PATH "resources/lingot-0_9_2b8.conf");
-    if (ok) { // TODO: assert
+    CU_ASSERT(ok);
 
 #ifdef PULSEAUDIO
-        CU_ASSERT_EQUAL(config->audio_system_index, lingot_audio_system_find_by_name("PulseAudio"));
-        CU_ASSERT(!strcmp(config->audio_dev[config->audio_system_index], "default"));
+    CU_ASSERT_EQUAL(config->audio_system_index, lingot_audio_system_find_by_name("PulseAudio"));
+    CU_ASSERT(!strcmp(config->audio_dev[config->audio_system_index], "default"));
 #endif
-        CU_ASSERT_EQUAL(config->sample_rate, 44100);
-        CU_ASSERT_EQUAL(config->root_frequency_error, 0.0);
-        CU_ASSERT_EQUAL(config->fft_size, 512);
-        CU_ASSERT_EQUAL(config->sample_rate, 44100);
-        CU_ASSERT_EQUAL(config->temporal_window, 0.3);
-        CU_ASSERT_EQUAL(config->min_overall_SNR, 20.0);
-        CU_ASSERT_EQUAL(config->calculation_rate, 15.0);
-        CU_ASSERT_EQUAL(config->min_frequency, 82.41);
-        CU_ASSERT_EQUAL(config->max_frequency, 329.63);
+    CU_ASSERT_EQUAL(config->sample_rate, 44100);
+    CU_ASSERT_EQUAL(config->root_frequency_error, 0.0);
+    CU_ASSERT_EQUAL(config->fft_size, 512);
+    CU_ASSERT_EQUAL(config->sample_rate, 44100);
+    CU_ASSERT_EQUAL(config->temporal_window, 0.3);
+    CU_ASSERT_EQUAL(config->min_overall_SNR, 20.0);
+    CU_ASSERT_EQUAL(config->calculation_rate, 15.0);
+    CU_ASSERT_EQUAL(config->min_frequency, 82.41);
+    CU_ASSERT_EQUAL(config->max_frequency, 329.63);
 
-        CU_ASSERT(!strcmp(config->scale.name, "Default equal-tempered scale"));
-        CU_ASSERT_EQUAL(config->scale.notes, 12);
-        CU_ASSERT_EQUAL(config->scale.base_frequency, 261.625565);
-        CU_ASSERT(!strcmp(config->scale.note_name[1], "C#"));
-        CU_ASSERT(!strcmp(config->scale.note_name[11], "B"));
+    CU_ASSERT(!strcmp(config->scale.name, "Default equal-tempered scale"));
+    CU_ASSERT_EQUAL(config->scale.notes, 12);
+    CU_ASSERT_EQUAL(config->scale.base_frequency, 261.625565);
+    CU_ASSERT(!strcmp(config->scale.note_name[1], "C#"));
+    CU_ASSERT(!strcmp(config->scale.note_name[11], "B"));
 
-        CU_ASSERT_EQUAL(config->scale.offset_ratios[0][0], 1);
-        CU_ASSERT_EQUAL(config->scale.offset_ratios[1][0], 1); // defined as ratio
-        CU_ASSERT_EQUAL(config->scale.offset_cents[0], 0.0);
+    CU_ASSERT_EQUAL(config->scale.offset_ratios[0][0], 1);
+    CU_ASSERT_EQUAL(config->scale.offset_ratios[1][0], 1); // defined as ratio
+    CU_ASSERT_EQUAL(config->scale.offset_cents[0], 0.0);
 
-        CU_ASSERT_EQUAL(config->scale.offset_ratios[0][1], -1);
-        CU_ASSERT_EQUAL(config->scale.offset_ratios[1][1], -1); // not defined as ratio
-        CU_ASSERT_EQUAL(config->scale.offset_cents[1], 100.0);  // defined as shift in cents
+    CU_ASSERT_EQUAL(config->scale.offset_ratios[0][1], -1);
+    CU_ASSERT_EQUAL(config->scale.offset_ratios[1][1], -1); // not defined as ratio
+    CU_ASSERT_EQUAL(config->scale.offset_cents[1], 100.0);  // defined as shift in cents
 
-        CU_ASSERT_EQUAL(config->scale.offset_ratios[0][11], -1);
-        CU_ASSERT_EQUAL(config->scale.offset_ratios[1][11], -1); // not defined as ratio
-        CU_ASSERT_EQUAL(config->scale.offset_cents[11], 1100.0); // defined as shift in cents
-    } else {
-        fprintf(stderr, "warning: cannot find unit test resources\n");
-    }
+    CU_ASSERT_EQUAL(config->scale.offset_ratios[0][11], -1);
+    CU_ASSERT_EQUAL(config->scale.offset_ratios[1][11], -1); // not defined as ratio
+    CU_ASSERT_EQUAL(config->scale.offset_cents[11], 1100.0); // defined as shift in cents
 
     // -----------
 
     ok = lingot_io_config_load(config, LINGOT_TEST_RESOURCES_PATH "resources/lingot-1_0_2b.conf");
-
     CU_ASSERT(ok);
 
     check_last_configs(config);
 
     // we check that we can save and load it again
-    char temp_filename[32];
-    strncpy(temp_filename,"/tmp/lingot-XXXXXX", sizeof(temp_filename));
 
-    int temp_file_id = mkstemps(temp_filename, 6);
-    CU_ASSERT(temp_file_id >= 0);
-    ok = close(temp_file_id);
-
-    CU_ASSERT(!ok);
-    ok = lingot_io_config_save(config, filename);
-    CU_ASSERT(ok != -1);
-
+    filename = tmpnam(NULL);
+    lingot_io_config_save(config, filename);
     ok = lingot_io_config_load(config, filename);
     CU_ASSERT(ok);
     check_last_configs(config);
@@ -192,11 +174,7 @@ void lingot_test_io_config(void) {
 
     // we check that we can save and load it again
 
-    temp_file_id = mkstemps(temp_filename, 6);
-    CU_ASSERT(temp_file_id >= 0);
-    ok = close(temp_file_id);
-
-    CU_ASSERT(!ok);
+    filename = tmpnam(NULL);
     lingot_io_config_save(config, filename);
     ok = lingot_io_config_load(config, filename);
     CU_ASSERT(ok);
